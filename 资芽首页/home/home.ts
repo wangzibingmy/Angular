7d78e21072e6c90f9879fd8db15cbc97e4a82d41
wangzibingmy <2119285613@qq.com>
2018-04-08 15:24:14 +0800

    资芽首页

---
+++ 资芽首页/home/home.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+    for (let i = 0; i < 3; i++) {
+      this.items.push( this.items.length );
+    }
+  }
+  search="infor";
+  items = [];
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+  
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.items.push( this.items.length );
+      }
+  
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+      // infiniteScroll.enable();
+    }, 500);
+  }
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //登录页
+  //public bk:ViewController
+  // ionViewDidLoad(){
+    // console.log('ionViewDidLoad');
+  //   this.navbar.backButtonClick=()=>{
+  //     this.navCtrl.pop();
+  //   }
+  //   this.bk.setBackButtonText('首页');
+  // }
+}
